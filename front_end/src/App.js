36165cca02460e8bbd66ace90273cc71b6b38ea1
jesintharnold <jesintharnold@gmail.com>
2022-09-10 13:28:16 +0530

    ✔ - Tweeter development started

--- front_end/src/App.js
+++ front_end/src/App.js
@@ -2,7 +2,7 @@ function App() {
 	return (
 		<div className="App">
 			<header className="App-header">
-				<h1 className="text-2xl text-red-700">Hello world!</h1>
+				<h1 className="text-2xl text-red-700">Hello-world!</h1>
 			</header>
 		</div>
 	);
