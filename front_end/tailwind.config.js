6487bc4eac18d82a063f582d90f1d13f93e661d5
jesintharnold <jesintharnold@gmail.com>
2022-09-10 13:44:26 +0530

    🛠 colors added for tweeter application

--- front_end/tailwind.config.js
+++ front_end/tailwind.config.js
@@ -7,6 +7,14 @@ module.exports = {
 			colors: {
 				"navbar": "#FFFFFF",
 				"main-background": "#333333",
+				"tweeter-blue-highlight":"#2F80ED",
+				"tweeter-grey":"#828282",
+				"tweeter-comment-box":"#FAFAFA",
+				"tweeter-likes":"#BDBDBD",
+				"tweeter-focus":"#F2F2F2",
+				"tweeter-green":"#27AE60",
+				"tweeter-red":"#EB5757",
+				"tweeter-blue":"#2D9CDB"
 			}
 		},
 	},
